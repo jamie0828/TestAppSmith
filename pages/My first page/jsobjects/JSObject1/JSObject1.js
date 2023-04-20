954ac62358281f98c2ff62e26688bd4b34541b3d
jamie0828 <jamie0828@163.com>
2023-04-20 08:22:50 +0000

    test2

---
+++ pages/My first page/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,15 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1: (a,b,c) => {
+		//write code here
+		console.log(a,b,c);
+	showAlert('I  AM CALLED '+a+' '+b+' '+c);
+		let aaa=12/0;
+		throw Error('I AM WRONG')
+		return 'OK'+aaa;
+	},
+	myFun2: async () => {
+		//use async-await or promises
+	}
+}
